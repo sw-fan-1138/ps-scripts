7285a6643dc258c522cfc5ded4069edaae806cf3
Anthony Sontag <anthonysontag@gmail.com>
2024-07-14 22:08:06 -0500

    Create saved_wifi_password.ps1

---
+++ saved_wifi_password.ps1
@@ -0,0 +1,9 @@
+#This script finds the password for a defined saved wifi network
+
+Write-Output "This will help you find the Wi-Fi password for a saved nework."
+
+Get-WiFiProfile
+Write-Output "Enter a Wi-Fi name under User Profiles from the list above"
+$wifinet=Read-Host "Wi-Fi Network SSID (Name): "
+Get-WiFiProfile -ProfileName $wifinet -ClearKey | Select-Object Password
+Write-Output "See above for your Wi-Fi password"
