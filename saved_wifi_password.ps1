fe0956a71f3c3fe19211f54150a201829a7a6d53
sw-fan-1138 <anthonysontag@gmail.com>
2025-01-01 18:58:09 -0600

    Updated saved_wifi_passwrd.ps1

--- saved_wifi_password.ps1
+++ saved_wifi_password.ps1
@@ -4,6 +4,6 @@ Write-Output "This will help you find the Wi-Fi password for a saved nework."
 
 Get-WiFiProfile
 Write-Output "Enter a Wi-Fi name under User Profiles from the list above"
-$wifinet=Read-Host "Wi-Fi Network SSID (Name): "
-Get-WiFiProfile -ProfileName $wifinet -ClearKey | Select-Object Password
+$wifiNet=Read-Host "Wi-Fi Network SSID (Name): "
+Get-WiFiProfile -ProfileName $wifiNet -ClearKey | Select-Object Password
 Write-Output "See above for your Wi-Fi password"
