309971e0e73c17243b13f7c70db616047f26ce14
sw-fan-1138 <anthonysontag@gmail.com>
2025-01-12 20:43:51 -0600

    Added comments in get_usb_parentoutput.ps1 and saved_wifi_password.ps1

--- get_usb_parent_output.ps1
+++ get_usb_parent_output.ps1
@@ -1,2 +1,3 @@
 # This PowerShell script retrieves all connected USB drives' DeviceID and outputs them into a text file. Useful for getting a list to import into MS Defender's USB device allow list.
+
 Get-WmiObject -Class Win32_PnPEntity | Where-Object Caption -Match "USB Mass Storage Device" | Select-Object -ExpandProperty DeviceID >> USBParents.txt
--- saved_wifi_password.ps1
+++ saved_wifi_password.ps1
@@ -1,9 +1,15 @@
 #This script finds the password for a defined saved wifi network
 
+#Lets user know what hte script does
 Write-Output "This will help you find the Wi-Fi password for a saved nework."
 
+#Shows wifi profiles saved to the computer
 Get-WiFiProfile
+
+#Asks user to enter wifi ProfileName
 Write-Output "Enter a Wi-Fi name under User Profiles from the list above"
 $wifiNet=Read-Host "Wi-Fi Network SSID (Name): "
+
+#Shows entered ProfileName password
 Get-WiFiProfile -ProfileName $wifiNet -ClearKey | Select-Object Password
 Write-Output "See above for your Wi-Fi password"
