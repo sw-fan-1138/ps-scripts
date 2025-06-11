029a980c7b3ee142c3da175fc5f67c8caa71f947
sw-fan-1138 <anthonysontag@gmail.com>
2025-06-10 21:03:50 -0500

    Added broken_bookmarks_checker.ps1 to README

--- broken_bookmarks_checker.ps1
+++ broken_bookmarks_checker.ps1
@@ -21,7 +21,7 @@ Get-ChildItem -LiteralPath $FilePath -Recurse -File "bookmarks.html" `
             }
             catch {
                 $Response = $PSItem.Exception.Response;
-                "$([int]$Response.StatusCode), $Value" >> .\Broken_Bookmarks.csv;
+                "$([int]$Response.StatusCode), $Value" >> .\broken_bookmarks.csv;
             }
         };
 };
