4ae66bc065f6f275ac65a623816b1a9b180d461a
sw-fan-1138 <anthonysontag@gmail.com>
2025-06-20 21:40:55 -0500

    Took account if the computer runs OneDrive or not and added a file picker dialog box.

--- broken_bookmarks_checker.ps1
+++ broken_bookmarks_checker.ps1
@@ -1,7 +1,39 @@
-#This script creates a csv file listing broken bookmark links from Firefox bookmarks
+#This script creates a csv file listing broken bookmark links from Firefox bookmarks.
+#Adapted from a script found here: https://swimburger.net/blog/powershell/powershell-script-scan-documentation-for-broken-links.
+
+#Checks if OneDrive is installed or not, and chooses the correct output path.
+if (Test-Path -path $Env:OneDrive)
+{
+    # Folder exists
+    $csvpath = $Env:OneDrive
+}
+else
+{
+    # Folder doesn't exists
+    $csvpath = $Env:USERPROFILE
+}
+
+#Opens a file picker dialog box to select your bookmarks file.
+Function Get-FileName($initialDirectory)
+{  
+ [System.Reflection.Assembly]::LoadWithPartialName(“System.windows.forms”) |
+ Out-Null
+
+ $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+ $OpenFileDialog.initialDirectory = "c.\\"
+ $OpenFileDialog.filter = "html files (*.html)|*.html|All files (*.*)|*.*";
+ $OpenFileDialog.FilterIndex = 1;
+ $OpenFileDialog.ShowDialog() | Out-Null
+ $OpenFileDialog.filename
+}
+#end function Get-FileName
+
+# *** Entry Point to Script ***
+
+$BookmarkFile = Get-FileName -initialDirectory “c:fso"
 
 # Url Regex specifically for HTML keeping []() [][] into account
-$FilePath = $env:APPDATA+"\Mozilla\Firefox\Profiles"
+$FilePath = $BookmarkFile
 $UrlRegex = '((?:https?):\/\/[a-z0-9\.:].*?(?=[\s\]\[\)]))|((?:https?):\/\/[a-z0-9\.:].*?(?=[\s\]\[\)]))';
 Get-ChildItem -LiteralPath $FilePath -Recurse -File "bookmarks.html" `
     | Select-String -Pattern $UrlRegex -AllMatches `
@@ -21,7 +53,7 @@ Get-ChildItem -LiteralPath $FilePath -Recurse -File "bookmarks.html" `
             }
             catch {
                 $Response = $PSItem.Exception.Response;
-                "$([int]$Response.StatusCode), $Value" >> $Env:OneDrive\Desktop\broken_bookmarks.csv;
+                "$([int]$Response.StatusCode), $Value" >> $csvpath\Desktop\broken_bookmarks.csv;
             }
         };
 };
