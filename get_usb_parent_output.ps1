a17b6a31a2fa970711974ca1e1becc896bd86fb6
Anthony Sontag <7761609+sw-fan-1138@users.noreply.github.com>
2025-07-02 08:40:25 -0500

    Update get_usb_parent_output.ps1

--- get_usb_parent_output.ps1
+++ get_usb_parent_output.ps1
@@ -1,3 +1,3 @@
-# This PowerShell script retrieves the DeviceID of all connected USB drives and outputs them to a text file. Useful for creating a list to import into Microsoft Defender's USB device allow list.
+# This PowerShell script retrieves the DeviceID of all connected USB drives and outputs them to a text file.
 
 Get-WmiObject -Class Win32_PnPEntity | Where-Object Caption -Match "USB Mass Storage Device" | Select-Object -ExpandProperty DeviceID >> USBParents.txt
